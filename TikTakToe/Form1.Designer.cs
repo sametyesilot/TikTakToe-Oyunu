a9837f1f6ca3d04ee279272420ed5e035decefe6
sametyesilot <144464600+sametyesilot@users.noreply.github.com>
2025-01-13 14:27:58 +0300

    TikTakToe Oyunu
    c# tiktaktoe oyunu

---
+++ TikTakToe/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace TikTakToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TikTakToe.Properties.Resources.pngkey_com_tic_tac_toe_png_2056247;
+            this.pictureBox1.Location = new System.Drawing.Point(98, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(595, 526);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(125, 136);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(153, 118);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(220, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 105);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "PLAYER 1";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(319, 136);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(153, 118);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(515, 136);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(153, 118);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Location = new System.Drawing.Point(125, 299);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(153, 118);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox11.TabIndex = 14;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Click += new System.EventHandler(this.pictureBox11_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(319, 299);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(153, 118);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 15;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(515, 299);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(153, 118);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 16;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(125, 458);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(153, 118);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 17;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(319, 457);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(153, 118);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox8.TabIndex = 18;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(515, 458);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(153, 118);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox9.TabIndex = 19;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 629);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+    }
+}
+
---
+++ TikTakToe/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TikTakToe
+{
+
+    
+    public partial class Form1 : Form
+    {
+        public int tiklamaSayisi = 0;
+        int[,] kontrol;
+        public Form1()
+        {
+            InitializeComponent();
+            kontrol = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    kontrol[i, j] = 0;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public string OyuncuKontol(int tiklanmasayisi)
+        {
+            if (tiklanmasayisi % 2 == 0)
+            {
+                
+                label1.Text = "PLAYER 1";
+                return @"C:\Users\Samet\source\repos\TikTakToe\TikTakToe\image\images1.png";
+            }
+            else
+            {
+                label1.Text = "PLAYER 2";
+                return @"C:\Users\Samet\source\repos\TikTakToe\TikTakToe\image\images2.png";
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+            tiklamaSayisi++;
+            string fotograf = @OyuncuKontol(tiklamaSayisi);
+            Atama(fotograf, 0, 0);
+            pictureBox2.Image = Image.FromFile(fotograf);
+            pictureBox2.Enabled = false;
+            KazananMesaj(Player1Kontrol());
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            tiklamaSayisi++;
+            string fotograf = @OyuncuKontol(tiklamaSayisi);
+            Atama(fotograf, 0, 1);
+            pictureBox3.Image = Image.FromFile(fotograf);
+            pictureBox3.Enabled = false;
+            KazananMesaj(Player1Kontrol());
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            tiklamaSayisi++;
+            string fotograf = @OyuncuKontol(tiklamaSayisi);
+            Atama(fotograf, 0, 2);
+            pictureBox4.Image = Image.FromFile(fotograf);
+            pictureBox4.Enabled = false;
+            KazananMesaj(Player1Kontrol());
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+            tiklamaSayisi++;
+            string fotograf = @OyuncuKontol(tiklamaSayisi);
+            Atama(fotograf, 1, 0);
+            pictureBox11.Image = Image.FromFile(fotograf);
+            pictureBox11.Enabled = false;
+            KazananMesaj(Player1Kontrol());
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            tiklamaSayisi++;
+            string fotograf = @OyuncuKontol(tiklamaSayisi);
+            Atama(fotograf, 1, 1);
+            pictureBox5.Image = Image.FromFile(fotograf);
+            pictureBox5.Enabled = false;
+            KazananMesaj(Player1Kontrol());
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            tiklamaSayisi++;
+            string fotograf = @OyuncuKontol(tiklamaSayisi);
+            Atama(fotograf, 1,2);
+            pictureBox6.Image = Image.FromFile(fotograf);
+            pictureBox6.Enabled = false;
+            KazananMesaj(Player1Kontrol());
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            tiklamaSayisi++;
+            string fotograf = @OyuncuKontol(tiklamaSayisi);
+            Atama(fotograf, 2, 0);
+            pictureBox7.Image = Image.FromFile(fotograf);
+            pictureBox7.Enabled = false;
+            KazananMesaj(Player1Kontrol());
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            tiklamaSayisi++;
+            string fotograf = @OyuncuKontol(tiklamaSayisi);
+            Atama(fotograf, 2, 1);
+            pictureBox8.Image = Image.FromFile(fotograf);
+            pictureBox8.Enabled = false;
+            KazananMesaj(Player1Kontrol());
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            tiklamaSayisi++;
+            string fotograf = @OyuncuKontol(tiklamaSayisi);
+            Atama(fotograf, 2, 2);
+            pictureBox9.Image = Image.FromFile(fotograf);
+            pictureBox9.Enabled = false;
+            KazananMesaj(Player1Kontrol());
+        }
+
+        public void Atama(string yol,int satir, int sutun)
+        {
+            if (yol.Equals(@"C:\Users\Samet\source\repos\TikTakToe\TikTakToe\image\images1.png"))
+            {
+                kontrol[satir, sutun] = 2;
+            }
+            else
+            {
+                kontrol[satir, sutun] = 1;
+            }
+        }
+        public int Player1Kontrol()
+        {
+            
+                // Yatayda kontrol
+                for (int i = 0; i < 3; i++)
+                {
+                    if (kontrol[i, 0] != 0 && kontrol[i, 0] == kontrol[i, 1] && kontrol[i, 1] == kontrol[i, 2])
+                    {
+                        return kontrol[i, 0]; // X için 1, O için 2
+                    }
+                }
+
+                // Dikeyde kontrol
+                for (int j = 0; j < 3; j++)
+                {
+                    if (kontrol[0, j] != 0 && kontrol[0, j] == kontrol[1, j] && kontrol[1, j] == kontrol[2, j])
+                    {
+                        return kontrol[0, j]; // X için 1, O için 2
+                    }
+                }
+
+                // Çaprazlarda kontrol
+                if (kontrol[0, 0] != 0 && kontrol[0, 0] == kontrol[1, 1] && kontrol[1, 1] == kontrol[2, 2])
+                {
+                    return kontrol[0, 0]; // X için 1, O için 2
+                }
+                if (kontrol[0, 2] != 0 && kontrol[0, 2] == kontrol[1, 1] && kontrol[1, 1] == kontrol[2, 0])
+                {
+                    return kontrol[0, 2]; // X için 1, O için 2
+                }
+
+                // Eğer kazanan yoksa
+                return 0;
+            
+        }
+        public void KazananMesaj(int kazanan)
+        {
+            
+            if (kazanan == 1)
+            {
+                MessageBox.Show("Player 1 kazandı!");
+                OyunuSifirla();
+            }
+            else if (kazanan == 2)
+            {
+                MessageBox.Show("Player 2 kazandı!");
+                OyunuSifirla();
+            }
+            else if (tiklamaSayisi == 9) // Eğer tüm hücreler doluysa ve kazanan yoksa
+            {
+                MessageBox.Show("Berabere!");
+                OyunuSifirla();
+            }
+        }
+        private void OyunuSifirla()
+        {
+            tiklamaSayisi = 0;
+            label1.Text = "PLAYER 1";
+            pictureBox2.Image = null;
+            pictureBox3.Image = null;
+            pictureBox4.Image = null;
+            pictureBox11.Image = null;
+            pictureBox5.Image = null;
+            pictureBox6.Image = null;
+            pictureBox7.Image = null;
+            pictureBox8.Image = null;
+            pictureBox9.Image = null;
+            pictureBox2.Enabled = true;
+            pictureBox3.Enabled = true;
+            pictureBox4.Enabled = true;
+            pictureBox11.Enabled = true;
+            pictureBox5.Enabled = true;
+            pictureBox6.Enabled = true;
+            pictureBox7.Enabled = true;
+            pictureBox8.Enabled = true;
+            pictureBox9.Enabled = true;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    kontrol[i, j] = 0; // Tüm hücreleri boş olarak ayarlıyoruz
+                }
+            }
+
+
+        }
+    }
+}
